7e4ab4fa82d3485eacf4219401b29d67c70f1cb4
azriel <azriel91@gmail.com>
2015-10-23 14:20:11 +1300

    Removed references to biicode
    - Changed BII_BLOCK_NAME to PROJECT_NAME
    - Removed BII_BLOCK_USER reference
    - Removed bii-support submodule
    - Removed build scripts that reference biicode

--- slBlockFunctions.cmake
+++ slBlockFunctions.cmake
@@ -3,7 +3,7 @@ set(SL_BLOCK_FUNCTIONS_DIR ${CMAKE_CURRENT_LIST_DIR})
 #======================================================================================================================#
 # [PUBLIC/USER]
 #
-# ADAPT_TARGETS_FOR_CPPMICROSERVICES()
+# adapt_targets_for_cppmicroservices()
 #
 # Adapt a block to a CppMicroServices module. This handles building the block as both static and shared libraries.
 #
@@ -14,15 +14,15 @@ function(ADAPT_TARGETS_FOR_CPPMICROSERVICES )
 
   set(target_name )
   if (${arg_count} EQUAL 0)
-    set(target_name ${BII_LIB_TARGET})
+    set(target_name ${PROJECT_NAME})
   else()
     list(GET macro_args 0 target_name)
   endif()
   message("Generating Block header and linking to target: ${target_name}")
 
-  SL_GENERATE_AND_LINK_BLOCK_HEADER(${target_name})
+  sl_generate_and_link_block_header(${target_name})
 
-  set_property(TARGET ${target_name} APPEND PROPERTY COMPILE_DEFINITIONS US_MODULE_NAME=${BII_BLOCK_NAME})
+  set_property(TARGET ${target_name} APPEND PROPERTY COMPILE_DEFINITIONS US_MODULE_NAME=${PROJECT_NAME})
 
   # Indicate this is a static module if we aren't building shared libraries
   if(NOT BUILD_SHARED_LIBS)
@@ -33,8 +33,8 @@ endfunction()
 #======================================================================================================================#
 # [PUBLIC/USER]
 #
-# SL_INCLUDE_TESTS(${BII_path_to_test1_TARGET}
-#                  ${BII_path_to_test2_TARGET}
+# sl_include_tests(testTarget1
+#                  testTarget2
 #                  ...)
 #
 # Includes the "test" directory if it exists
@@ -42,7 +42,7 @@ endfunction()
 #======================================================================================================================#
 function(SL_INCLUDE_TESTS )
   set(TEST_DIR "${CMAKE_CURRENT_SOURCE_DIR}/test")
-  if(NOT ${BII_IS_DEP} AND EXISTS ${TEST_DIR} AND IS_DIRECTORY ${TEST_DIR})
+  if(EXISTS ${TEST_DIR} AND IS_DIRECTORY ${TEST_DIR})
 
     foreach(TEST_TARGET ${ARGN})
       # Need to tell GTest not to use std::tuple because CppMicroServices uses its own definition, and it causes a
@@ -57,10 +57,9 @@ endfunction()
 #======================================================================================================================#
 # [PRIVATE/INTERNAL]
 #
-# SL_GENERATE_AND_LINK_BLOCK_HEADER(target_name)
+# sl_generate_and_link_block_header(target_name)
 #
-# Generates a "Block.h" header file that contains the c++ library export definition and sets 'ns' as an alias for the
-# block namespace.
+# Generates a "Block.h" header file that contains the c++ library export definition.
 #
 # For example, for a block called sl_core_application:
 # - the export macro is SL_CORE_APPLICATION_EXPORT
@@ -68,23 +67,23 @@ endfunction()
 #
 #======================================================================================================================#
 function(SL_GENERATE_AND_LINK_BLOCK_HEADER target_name)
-  string(TOUPPER ${BII_BLOCK_NAME} BII_BLOCK_NAME_UPPER) # used in Block.h.in
-  string(REGEX MATCHALL "([^_]+)+" BII_BLOCK_NAME_SEGMENTS ${BII_BLOCK_NAME})
+  string(TOUPPER ${PROJECT_NAME} PROJECT_NAME_UPPER) # used in Block.h.in
+  string(REGEX MATCHALL "([^_]+)+" PROJECT_NAME_SEGMENTS ${PROJECT_NAME})
 
   # Generate the namespace <segment> {} declarations
   set(BLOCK_NAMESPACE_DECLARATION "")
-  foreach(SEGMENT ${BII_BLOCK_NAME_SEGMENTS})
+  foreach(SEGMENT ${PROJECT_NAME_SEGMENTS})
     string(CONCAT BLOCK_NAMESPACE_DECLARATION "${BLOCK_NAMESPACE_DECLARATION}" "namespace ${SEGMENT} {\n")
   endforeach()
-  foreach(SEGMENT ${BII_BLOCK_NAME_SEGMENTS})
+  foreach(SEGMENT ${PROJECT_NAME_SEGMENTS})
     string(CONCAT BLOCK_NAMESPACE_DECLARATION "${BLOCK_NAMESPACE_DECLARATION}" "}\n")
   endforeach()
 
   # Generate the header file
   configure_file(${SL_BLOCK_FUNCTIONS_DIR}/Block.h.in
-                 ${CMAKE_CURRENT_BINARY_DIR}/${BII_BLOCK_NAME}/Block.h)
-  set(SL_BLOCK_HEADER_TARGET "${BII_BLOCK_NAME}_BLOCK_HEADER")
-  add_custom_target(${SL_BLOCK_HEADER_TARGET} DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${BII_BLOCK_NAME}/Block.h)
+                 ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}/Block.h)
+  set(SL_BLOCK_HEADER_TARGET "${PROJECT_NAME}_BLOCK_HEADER")
+  add_custom_target(${SL_BLOCK_HEADER_TARGET} DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}/Block.h)
 
   # Add the block header target as a dependency of the specified target
   add_dependencies(${target_name} ${SL_BLOCK_HEADER_TARGET})
